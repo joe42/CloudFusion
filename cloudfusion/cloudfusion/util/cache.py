fb335794a2b6d301a2bf1a91de213cf7c5dac986
Johannes Müller <dna42@gmx.de>
2013-02-10 22:31:13 +0100

    Modifify cache.py         Refactor dictionary used for cache entries into new class Entry         Refactor methods update() and set_dirty() of class Cache, turning dictionary changes into explicit assignment                 This is necssary so that the dictionary storing the cache entries can be replaced by a shelve (with writeback=False property) for persistance 	Refactor methods get_size_of_dirty_data() and get_size_of_cached_data() to use _get_size_of_entry(entry) 	Add method _get_size_of_entry(entry) to class Cache, which can be overridden in subclasses

--- cloudfusion/cloudfusion/util/cache.py
+++ cloudfusion/cloudfusion/util/cache.py
@@ -5,61 +5,59 @@ Created on 03.06.2011
 '''
 import time
 
+class Entry(object):
+    '''
+    Cache entry.
+    '''
+    def __init__(self, value, dirty, modified=None, updated=None):
+        self.value = value
+        self.dirty = dirty
+        self.updated = updated if updated != None else time.time()
+        self.modified = modified if modified != None else time.time()
+
 class Cache(object):
     def __init__(self, expiration_time):
+        """Return a Cache instance with entries expiring after :param:`expiration_time` seconds."""
         self.entries = {}
         self.expire = expiration_time
         
     def refresh(self, key, disk_value, modified):
-        """ Refreshes a entries entry with :param:`disk_value`, if :param:`modified` is bigger than the entries entry's modified date. """
+        """ Refreshes an entry with :param:`disk_value`, if :param:`modified` is bigger than the entry's modified date. """
         if key in self.entries:
-            disk_entry_is_newer = modified > self.entries[key]['modified']
+            disk_entry_is_newer = modified > self.entries[key].modified
             if not disk_entry_is_newer:
                 return
-        entry = {}
-        entry['value'] = disk_value
-        entry['updated'] = time.time()
-        entry['modified'] = modified
-        entry['dirty'] = False
-        self.entries[key] = entry
+        self.entries[key] = Entry(value=disk_value, dirty=False, modified=modified)
         
     def write(self, key, value):
-        entry = {}
-        entry['value'] = value
-        entry['updated'] = time.time()
-        entry['modified'] = time.time()
-        entry['dirty'] = True
-        self.entries[key] = entry
+        self.entries[key] = Entry(value=value, dirty=True)
         
     def get_keys(self):
         return self.entries.keys()
     
     def get_modified(self, key):
-        return self.entries[key]['modified']
+        return self.entries[key].modified
+    
+    def _get_size_of_entry(self, entry):
+        try:
+            return entry.value.get_size()
+        except:
+            try:
+                return entry.value.size
+            except:
+                return len(str(entry.value))
     
     def get_size_of_dirty_data(self):
         ret = 0
-        for entry in self.entries:
-            if self.is_dirty(entry):
-                try:
-                    ret+= self.get_value(entry).get_size()
-                except:
-                    try:
-                        ret+= self.get_value(entry).size
-                    except:
-                        ret += len(str(self.get_value(entry)))
+        for key in self.entries:
+            if self.is_dirty(key):
+                ret+= self._get_size_of_entry(self.entries[key])
         return ret
     
     def get_size_of_cached_data(self):
         ret = 0
-        for entry in self.entries:
-                try:
-                    ret+= self.get_value(entry).get_size()
-                except:
-                    try:
-                        ret+= self.get_value(entry).size
-                    except:
-                        ret += len(str(self.get_value(entry)))
+        for key in self.entries:
+            ret+= self._get_size_of_entry(self.entries[key])
         return ret
     
     def exists(self, key):
@@ -68,23 +66,27 @@ class Cache(object):
         return False
 
     def is_expired(self, key):
-        return time.time() > self.entries[key]['updated'] + self.expire
+        return time.time() > self.entries[key].updated + self.expire
     
     def update(self, key):
-        self.entries[key]['updated'] = time.time() 
+        entry = self.entries[key]
+        entry.updated = time.time() 
+        self.entries[key] = entry
         
     def flush(self, key):
         self.update(key) 
         self.set_dirty(key, False) 
     
     def get_value(self, key):
-        return self.entries[key]['value']
+        return self.entries[key].value
     
     def is_dirty(self, key):
-        return self.entries[key]['dirty']
+        return self.entries[key].dirty
     
     def set_dirty(self, key, is_dirty):
-        self.entries[key]['dirty'] = is_dirty
+        entry = self.entries[key]
+        entry.dirty = is_dirty
+        self.entries[key] = entry
     
     def delete(self, key):
         try:
