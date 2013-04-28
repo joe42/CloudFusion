joe42/CloudFusion

commit 9187d552d9ea71a61fd49392dff08c42d1b74ebb

Add chunked upload for DropboxStore, to be able to store large files

cloudfusion/cloudfusion/store/dropbox/dropbox_store.py
@@ -98,18 +98,35 @@ class DropboxStore(Store):
     def store_fileobject(self, fileobject, path):
         self.logger.debug("storing file object to "+path)
         remote_file_name = os.path.basename(path)
-        namable_file = NameableFile( fileobject, remote_file_name )
+        fileobject.seek(0,2)
+        size = fileobject.tell()
+        nameable_file = NameableFile( fileobject, remote_file_name )
+        uploader = self.client.get_chunked_uploader(nameable_file, size)
+        retry = 5
+        while uploader.offset < size:
+            try:
+                upload = uploader.upload_chunked()
+            except rest.ErrorResponse, e:
+                retry -= 1
+                if retry == 0:
+                    msg= "could not store file: " +path+remote_file_name 
+                    self._log_http_error("store_fileobject", path, resp, msg)
+                    raise StoreAccessError("Transfer error: "+str(e), 0)
+            except Exception, e:
+                raise StoreAccessError("Transfer error: "+str(e), 0)
         try:
-            resp = self.client.put_file(path, namable_file) 
+            resp = self.client.finish(path, overwrite=True)
         except Exception, e:
             try:
-                resp = self.client.put_file(path, namable_file) 
+                resp = self.client.finish(path, overwrite=True)
             except rest.ErrorResponse as resp:
                 msg= "could not store file: " +path+remote_file_name 
                 self._log_http_error("store_fileobject", path, resp, msg)
                 raise StoreAccessError("Transfer error: "+str(e), 0)
             except Exception, e:
                 raise StoreAccessError("Transfer error: "+str(e), 0)
+        return resp #return metadata
+    
     
     def delete(self, path):
         self.logger.debug("deleting " +path)

commit 91d04604e5757da64ca083169ed032f45a0f4706

Fix bug introduced in 9187d55     Fix method store_fileobject of class DropboxStore:         use method put_file from dropboxclient instead of chunkeduploader, for empty or small files - chunkeduploader does not handle empty files

cloudfusion/cloudfusion/store/dropbox/dropbox_store.py
@@ -94,6 +94,24 @@ class DropboxStore(Store):
                 raise StoreAccessError("Transfer error: "+str(e), 0)
         ret = resp.read()
         return ret
+    
+    def store_small_fileobject(self, fileobject, path):
+        self.logger.debug("storing file object to "+path)
+        remote_file_name = os.path.basename(path)
+        namable_file = NameableFile( fileobject, remote_file_name )
+        try:
+            resp = self.client.put_file(path, namable_file)
+        except Exception, e:
+            try:
+                resp = self.client.put_file(path, namable_file)
+            except rest.ErrorResponse as resp:
+                msg= "could not store file: " +path+remote_file_name
+                self._log_http_error("store_fileobject", path, resp, msg)
+                raise StoreAccessError("Transfer error: "+str(e), 0)
+            except Exception, e:
+                raise StoreAccessError("Transfer error: "+str(e), 0)   
+        return resp 
+            
 
     def __get_size(self, fileobject):
         pos = fileobject.tell()
@@ -106,6 +124,8 @@ class DropboxStore(Store):
         self.logger.debug("storing file object to "+path)
         remote_file_name = os.path.basename(path)
         size = self.__get_size(fileobject)
+        if size < 100000:
+            return self.store_small_fileobject(fileobject, path)
         nameable_file = NameableFile( fileobject, remote_file_name )
         uploader = self.client.get_chunked_uploader(nameable_file, size)
         retry = 5