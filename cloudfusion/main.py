d3e84136d0a8c866be98d43ccca6d1341243c12d
Johannes Müller <dna42@gmx.de>
2013-08-07 18:05:36 +0200

    Improve performance by adding fuse options big_write, max_read, and max_write     Modify main.py

--- cloudfusion/main.py
+++ cloudfusion/main.py
@@ -11,15 +11,21 @@ from cloudfusion.mylogging.nullhandler import NullHandler
 import cloudfusion
 
 def check_arguments(args):
-    if not len(args) in [2,3,4]:
-        print 'usage: %s mountpoint [foreground] [log]' % args[0]
+    if not len(args) in [2,3,4,5]:
+        print 'usage: %s mountpoint [foreground] [log] [big_writes]' % args[0]
+        print 'foreground: run program in foreground'
+        print 'log: write logs to the directory .cloudfusion/logs'
+        print 'big_writes: enable better throughput with fuse (requires fuse 2.8 or higher)'
         exit(1)
 
 def main():
     check_arguments(sys.argv)
     foreground  = False 
+    big_writes = {}
     if "foreground" in sys.argv:
         foreground = True
+    if "big_writes" in sys.argv: #set parameters for big writes to fuse
+        big_writes = {"big_writes":True, "max_read":131072, "max_write":131072}
     if not "log" in sys.argv:
         logging.getLogger().addHandler(NullHandler())
     else:
@@ -33,7 +39,7 @@ def main():
     if not os.path.exists(sys.argv[1]):
         os.makedirs(sys.argv[1])
     fuse_operations = ConfigurablePyFuseBox(sys.argv[1])
-    FUSE(fuse_operations, sys.argv[1], foreground=foreground, nothreads=True)
+    FUSE(fuse_operations, sys.argv[1], foreground=foreground, nothreads=True, **big_writes)
     
 if __name__ == '__main__':
     main()
