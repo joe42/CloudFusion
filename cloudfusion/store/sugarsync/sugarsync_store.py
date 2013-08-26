83b2772185f3917abaa8e3fa2de3383f996ff27b
Johannes Müller <dna42@gmx.de>
2013-08-26 14:56:12 +0200

    Modify method store_file of class SugarsyncStore, to use Process instead of apply_async, because I thought this would fix the problem with the processes not terminating, which is not the case (btw. apply_async needs multiprocessing.Manager().Event())

--- cloudfusion/store/sugarsync/sugarsync_store.py
+++ cloudfusion/store/sugarsync/sugarsync_store.py
@@ -103,7 +103,8 @@ exception UnimplementedDigestAuthOptionError
 exception UnimplementedHmacDigestAuthOptionError
     The server requested a type of HMACDigest authentication that we are unfamiliar with. """
         
-
+def wait_for_event(event):
+    event.wait()
  
 class SugarsyncStore(Store):
     def __init__(self, config):
@@ -237,99 +238,53 @@ class SugarsyncStore(Store):
             HTTP_STATUS.generate_exception(resp.status, str(resp))
         return resp.data 
     
-    def _store_fileobject(self, fileobject, translated_path, result, interrupt_event=None):
-        try:
-            resp = self.client.put_file( fileobject, translated_path )
-            return resp
-        except Exception, e:
-            return e
-         
-    def mystore(self, path_to_file, path):
-        fileobject = open(path_to_file)
-        self.client.put_file(fileobject, self._translate_path(path_to_file))
-    
     # retry does not really matter with caching_store
     @retry((Exception,socket.error), tries=1, delay=0) 
     def store_file(self, path_to_file, dest_dir="/", remote_file_name = None, interrupt_event=None):
-        path = dest_dir + "/" + remote_file_name
-        self.logger.debug("storingYY file object to %s", path)
-        
         if not remote_file_name:
             remote_file_name = os.path.basename(path_to_file)
-        if not self.exists(path_to_file):
-            self._create_file(path_to_file)
+        remote_path = dest_dir + "/" + remote_file_name
+        self.logger.debug("storing file to %s", remote_path)
+        if not self.exists(remote_path):
+            self._create_file(remote_path)
             
-        self.logger.debug("pool")
-        pool = multiprocessing.Pool(processes=2)
-        import pickle  
-        self.logger.debug("dmp path")
-        pickle.dumps(self._translate_path(path_to_file))
-        self.logger.debug("dmp putfile")
-        pickle.dumps(self.client.put_file)
-        self.logger.debug("11111111111111111")
-        resp = pool.apply_async(self.mystore, args=(path_to_file, self._translate_path(path_to_file)))
-        self.logger.debug("22222222222222222")
-        interrupted = None
+        result_queue = multiprocessing.Queue()
+        write_process = multiprocessing.Process(target=self.client.put_file_async, args=(path_to_file, self._translate_path(remote_path), result_queue))
+        write_process.daemon = True
+        write_process.start()
+        interrupt_process = None
         if interrupt_event:
-            interrupted = pool.apply_async(interrupt_event.wait, args=())
+            interrupt_process = multiprocessing.Process(target=wait_for_event, args=(interrupt_event,))
+            interrupt_process.daemon = True
+            interrupt_process.start()
         while True:
-            resp.wait(1)
-            if resp.ready():
-                self.logger.debug("finishing process %s", os.getpid())
-                pool.terminate()
-                self.logger.debug("finished process %s", os.getpid())
+            write_process.join(1)
+            if not write_process.is_alive():
+                if interrupt_process:
+                    interrupt_process.terminate()
                 break
-            if interrupted and interrupted.ready():
-                self.logger.debug("terminating process %s", os.getpid())
-                pool.terminate()
-                self.logger.debug("terminated process %s", os.getpid())
-                raise InterruptedException()
-            
+            if interrupt_process and not interrupt_process.is_alive():
+                self.logger.debug("terminating stale upload of %s in process %s", remote_path, write_process.pid)
+                write_process.terminate()
+                if interrupt_process:
+                    interrupt_process.terminate()
+                self.logger.debug("terminated stale upload of %s in process %s", remote_path, os.getpid())
+                raise InterruptedException("Stale upload has been interrupted.")
+        
+        resp = result_queue.get()   
         if isinstance(resp, Exception):
             raise resp
         
         if not resp.status in HTTP_STATUS.OK:
-            self.logger.warning("could not store file to %s\nstatus: %s reason: %s", path_to_file, resp.status, resp.reason)
+            self.logger.warning("could not store file to %s\nstatus: %s reason: %s", remote_path, resp.status, resp.reason)
             HTTP_STATUS.generate_exception(resp.status, str(resp))
             
-    # retry does not really matter with caching_store
     @retry((Exception,socket.error), tries=1, delay=0) 
-    def store_fileobject(self, fileobject, path_to_file, interrupt_event=None):
-        self.logger.debug("storingXX file object to %s", path_to_file)
+    def store_fileobject(self, fileobject, path_to_file):
+        self.logger.debug("storing file object to %s", path_to_file)
         if not self.exists(path_to_file):
             self._create_file(path_to_file)
-            
-        self.logger.debug("pool")
-        pool = multiprocessing.Pool(processes=2)
-        import pickle 
-        self.logger.debug("dmp fileobj")
-        pickle.dumps(fileobject)
-        self.logger.debug("dmp path")
-        pickle.dumps(self._translate_path(path_to_file))
-        self.logger.debug("dmp putfile")
-        pickle.dumps(self.client.put_file)
-        self.logger.debug("11111111111111111")
-        resp = pool.apply_async(self.client.put_file, args=(fileobject, self._translate_path(path_to_file)))
-        self.logger.debug("22222222222222222")
-        interrupted = None
-        if interrupt_event:
-            interrupted = pool.apply_async(interrupt_event.wait, args=())
-        while True:
-            resp.wait(1)
-            if resp.ready():
-                self.logger.debug("finishing process %s", os.getpid())
-                pool.terminate()
-                self.logger.debug("finished process %s", os.getpid())
-                break
-            if interrupted and interrupted.ready():
-                self.logger.debug("terminating process %s", os.getpid())
-                pool.terminate()
-                self.logger.debug("terminated process %s", os.getpid())
-                raise InterruptedException()
-            
-        if isinstance(resp, Exception):
-            raise resp
-        
+        resp = self.client.put_file( fileobject, self._translate_path(path_to_file) ) 
         if not resp.status in HTTP_STATUS.OK:
             self.logger.warning("could not store file to %s\nstatus: %s reason: %s", path_to_file, resp.status, resp.reason)
             HTTP_STATUS.generate_exception(resp.status, str(resp))
