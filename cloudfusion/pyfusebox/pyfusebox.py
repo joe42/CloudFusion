joe42/CloudFusion

commit a20a2028ffec4f6f696604a0075ad1c5a5f71913

Bugfix method truncate of class PyFuseBox     truncate needs to be able to extend files to larger size by adding nullbytes

cloudfusion/pyfusebox/pyfusebox.py
@@ -83,16 +83,28 @@ class PyFuseBox(Operations):
     
     def truncate(self, path, length, fh=None):
         self.logger.debug("truncate %s to %s" % (path, length))
-        try:
-            self.store.delete(path)
-        except NoSuchFilesytemObjectError:
-            raise FuseOSError(ENOENT)
-        except StoreAccessError:
-            raise FuseOSError(EIO)
-        except StoreAutorizationError:
-            raise FuseOSError(EACCES) 
-        temp_file = tempfile.SpooledTemporaryFile()
-        self.store.store_fileobject(temp_file, path)
+        if not path in self.temp_file:
+            data = ""
+            self.temp_file[path] = tempfile.SpooledTemporaryFile()
+            if self.store.get_max_filesize() < length:
+                self._release(path, 0) #to prevent flushing
+                return FuseOSError(EFBIG)
+            try:
+                data = self.store.get_file(path)
+            except NoSuchFilesytemObjectError:
+                raise FuseOSError(ENOENT)
+            except StoreAccessError:
+                raise FuseOSError(EIO)
+            except StoreAutorizationError:
+                raise FuseOSError(EACCES) 
+        else:
+            data = self.temp_file[path].read()
+        self.temp_file[path].truncate()
+        self.temp_file[path].write(data[:length])
+        padding = length - len(data)
+        if padding > 0:
+            self.temp_file[path].write('\x00'*padding)
+        self.temp_file[path].seek(0)
         return 0
     
     def rmdir(self, path):

commit 2b14c53bf0ed5af817a6300007cb8efe46a68e26

Bugfix method truncate of class PyFuseBox, check if file size exceeds maximum, bug introduced in commit a20a2028ffec4

cloudfusion/pyfusebox/pyfusebox.py
@@ -81,12 +81,12 @@ class PyFuseBox(Operations):
     
     def truncate(self, path, length, fh=None):
         self.logger.debug("truncate %s to %s", path, length)
+        if self.store.get_max_filesize() < length:
+            self._release(path, 0) #to prevent flushing
+            return FuseOSError(EFBIG)
         if not path in self.temp_file:
             data = ""
             self.temp_file[path] = tempfile.SpooledTemporaryFile(max_size=20*1000*1000)
-            if self.store.get_max_filesize() < length:
-                self._release(path, 0) #to prevent flushing
-                return FuseOSError(EFBIG)
             try:
                 data = self.store.get_file(path)
             except NoSuchFilesytemObjectError: