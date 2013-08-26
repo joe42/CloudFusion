joe42/CloudFusion

commit b11e794e54688a6a20f4eaa787038ac559fb48b8

Inform user when file is too large to handle by individual store     Modify method write of class PyFuseBox, to throw EFBIG if file is too large     Add method get_max_filesize to interface Store, classes CachingStore, MetadataCachingStore, DropboxStore, and SugarsyncStore, to return maximum file size it can handle in bytes

cloudfusion/pyfusebox/pyfusebox.py
@@ -217,6 +217,10 @@ class PyFuseBox(Operations):
         self.logger.debug("write %s ... starting with %s at %s - fh: %s" % (path, buf[0:10], offset, fh))
         if not path in self.temp_file:
             self.temp_file[path] = tempfile.SpooledTemporaryFile()
+            filesize = offset+len(buf)
+            if self.store.get_max_filesize() < filesize:
+                self.release(path, 0) #to prevent flushing
+                return FuseOSError(EFBIG)
             try:
                 file = self.store.get_file(path)
             except NoSuchFilesytemObjectError:

cloudfusion/store/caching_store.py
@@ -483,3 +483,7 @@ class MultiprocessingCachingStore(Store):
     
     def reconnect(self):
         self.store.reconnect()
+        
+    def get_max_filesize(self):
+        """Return maximum number of bytes per file"""
+        return self.store.get_max_filesize()

cloudfusion/store/dropbox/dropbox_store.py
@@ -457,6 +457,9 @@ class DropboxStore(Store):
     def get_logging_handler(self):
         return self._logging_handler
     
+    def get_max_filesize(self):
+        """Return maximum number of bytes per file"""
+        return 150*1000*1000
         
 
 

cloudfusion/store/metadata_caching_store.py
@@ -307,3 +307,7 @@ class MetadataCachingStore(Store):
             else:
                 setattr(result, k, deepcopy(v, memo))
         return result
+    
+    def get_max_filesize(self):
+        """Return maximum number of bytes per file"""
+        return self.store.get_max_filesize()

cloudfusion/store/store.py
@@ -125,3 +125,7 @@ class Store(object):
     
     def reconnect(self):
         pass
+    
+    def get_max_filesize(self):
+        """Return maximum number of bytes per file"""
+        return 1000*1000*1000

cloudfusion/store/sugarsync/sugarsync_store.py
@@ -332,3 +332,7 @@ class SugarsyncStore(Store):
         
     def get_logging_handler(self):
         return self._logging_handler
+    
+    def get_max_filesize(self):
+        """Return maximum number of bytes per file"""
+        return 100*1000*1000

commit 43cb7948d7735c93a876216d2f1aebd3b50dcc9a

Bugfix method write of class PyFuseBox, check if file size exceeds maximum, bug introduced in commit b11e794e54688a

cloudfusion/pyfusebox/pyfusebox.py
@@ -224,13 +224,13 @@ class PyFuseBox(Operations):
 
     def write(self, path, buf, offset, fh):
         #self.logger.debug("write %s ... starting with %s at %s - fh: %s", path, buf[0:10], offset, fh)
+        filesize = offset+len(buf)
+        if self.store.get_max_filesize() < filesize: 
+            self._release(path, 0) #to prevent flushing
+            return FuseOSError(EFBIG)
         if not path in self.temp_file:
             self.logger.debug("first write to %s ... starting with %s at %s - fh: %s", path, buf[0:10], offset, fh)
             self.temp_file[path] = tempfile.SpooledTemporaryFile(max_size=20*1000*1000)
-            filesize = offset+len(buf)
-            if self.store.get_max_filesize() < filesize:
-                self._release(path, 0) #to prevent flushing
-                return FuseOSError(EFBIG)
             try:
                 data = self.store.get_file(path)
             except NoSuchFilesytemObjectError: