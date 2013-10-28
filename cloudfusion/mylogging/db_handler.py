89c8c893469ac2a3603ceae85b757daf640e12b8
Johannes Müller <dna42@gmx.de>
2013-10-28 11:13:11 +0100

    Modify class DBHandler to not create a database table on creation; this should be done on db_logging_thread.start() Bugfix method emit of class DBHandler, retry if database is locked - and use autocommit mode Modify class DBHandler to Drop lost logging messages to stderr Modify class DBHandler to check if logging thread is  started

--- cloudfusion/mylogging/db_handler.py
+++ cloudfusion/mylogging/db_handler.py
@@ -3,6 +3,7 @@ import logging.handlers
 import sqlite3
 import pickle
 import os
+from time import sleep
 
 class DBHandler(logging.Handler):
     """
@@ -13,14 +14,15 @@ class DBHandler(logging.Handler):
         """:param db_identifier: identifier from :func:`cloudfusion.mylogging.db_logging_thread.get_logging_db_identifier`"""
         logging.Handler.__init__(self)
         self.db_identifier = db_identifier
+        if not db_identifier:
+            import sys 
+            sys.stderr.write("Logging handler DBHandler was initialized before calling cloudfusion.mylogging.db_logging_thread.start(). Nothing will be logged.\n")
         self.conn = None
         self.cursor = None
-        self.pid = os.getpid()
-        self.reconnect()
-        self.cursor.execute("CREATE TABLE IF NOT EXISTS logging (record BLOB);")
+        self.pid = -1
         
     def reconnect(self):
-        self.conn = sqlite3.connect(self.db_identifier, 60, check_same_thread = False) 
+        self.conn = sqlite3.connect(self.db_identifier, check_same_thread = False, isolation_level=None) 
         self.cursor = self.conn.cursor()
  
     def emit(self, record):
@@ -29,7 +31,9 @@ class DBHandler(logging.Handler):
 
         Writes the LogRecord to the database.
         """
-        if self.pid != os.getpid(): #reconnect if dbhandler is in new process fork
+        if not self.db_identifier:
+            return
+        if self.pid != os.getpid(): #reconnect if dbhandler is in new process fork or if it is not yet connected
             self.reconnect()
             self.pid = os.getpid()
         ei = record.exc_info
@@ -40,7 +44,17 @@ class DBHandler(logging.Handler):
         s = pickle.dumps(record)
         if ei:
             record.exc_info = ei # for next handler
-        self.cursor.execute("INSERT INTO logging (record) VALUES (?);", (sqlite3.Binary(s),))
-        self.conn.commit()
+        retry = True
+        try_cnt = 0
+        while retry:
+            try:
+                try_cnt += 1
+                self.cursor.execute("INSERT INTO logging (record) VALUES (?);", (sqlite3.Binary(s),))
+                retry = False
+            except sqlite3.OperationalError, e:
+                if try_cnt > 4:
+                    import sys
+                    sys.stderr.write("Lost logging message: %s\n" % self.format(record))
+                    break
         
     
