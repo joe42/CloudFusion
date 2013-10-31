2402b81198cc808c9aa9b81f3863659dccb0e492
Johannes Müller <dna42@gmx.de>
2013-10-31 19:26:43 +0100

    Add script .travis.yml, so that a build is automatically run, tested, and if successful published to github by travis.ci Add script run_tests.sh to run all tests. The script is stopped if a command returns a non-zero status. Add script generate_documentation.sh to generate documentation, and publish it to github into the gh-pages branch, if all tests pass, and the current commit contains a tag. The script is stopped when a command returns non-zero status. Add script publish_developer_branch.sh, to publish developer branch to the master branch, if all tests pass, and the current commit contains a tag. The script is stopped if a command returns non-zero status.

---
+++ buildscripts/publish_developer_branch.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#Publishes development branch to master, if it contains a tag. 
+#This means you need to push with --tags option from your local repository. Otherwise the tags are not pushed.
+#i.e. git tag -a v1.4;git push --tags 
+
+# exit if any command returns non-zero status
+set -e 
+
+wd=`pwd`
+
+##Prepare
+#configure git user for push
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis" 
+
+cd $HOME
+git clone --quiet --branch=development https://${GH_TOKEN}@github.com/joe42/CloudFusion.git development > /dev/null
+cd development
+
+
+{ git describe --contains HEAD; has_tag=$? || true; } # || true prevents returning non-zero status if the last commit does not have a tag assigned to it, and hence stop script execution
+if [ $has_tag == 0 ] ; then   #return value of git describe is zero if current commit has a tag
+    git push -q origin development:master >/dev/null
+fi
+
+#clean up
+cd $HOME
+rm -fr development
+cd $wd
