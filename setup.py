0a5bd365f5f6cd358aa76e58086a2ff1ceddd596
Johannes Müller <dna42@gmx.de>
2013-11-05 22:30:15 +0100

    Add module requests locally, to prevent the cookie error described in module dropbox_store, which does not occur in version 1.2.3  of module requests (fix Issue #9) Modify module dropbox_store, to use local  module request Modify script setup.py, to install current version of requests globally

--- setup.py
+++ setup.py
@@ -8,11 +8,7 @@ setuptools.setup(
     name = "CloudFusion",
     packages = setuptools.find_packages(),
     include_package_data = True,
-    install_requires = ['mechanize', 'requests==1.2.3', 'nose', 'oauth', 'poster', 'simplejson', 'httplib2', 'beautifulsoup4'],
-#  requests bug with requests 2.0.1:
-#    File "/usr/local/lib/python2.7/dist-packages/requests/cookies.py", line 311, in _find_no_duplicates
-#    raise KeyError('name=%r, domain=%r, path=%r' % (name, domain, path))
-#    KeyError: "name=Cookie(version=0, name='bang', value='QUFCQmh5c3FET1RnMUZkcXlrMXNBNXV4eFhaU080NWtzYndmUDlDa0p1SEFHZw%3D%3D', port=None, port_specified=False, domain='.dropbox.com', domain_specified=True, domain_initial_dot=False, path='/', path_specified=True, secure=False, expires=1383395405, discard=False, comment=None, comment_url=None, rest={'httponly': None}, rfc2109=False), domain=None, path=None"
+    install_requires = ['mechanize', 'requests', 'nose', 'oauth', 'poster', 'simplejson', 'httplib2', 'beautifulsoup4'],
     version = "3.7.6",
     description = "Filesystem interface to cloud storage services",
     author = "Johannes Mueller",
