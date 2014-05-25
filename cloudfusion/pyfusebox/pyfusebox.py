joe42/CloudFusion

commit c7621bf22761de3746e801ed65698475b4c5dbeb

Modify method rename of class PyFuseBox, to throw an error when trying move a directory so that it would overwrite an existing directory

cloudfusion/pyfusebox/pyfusebox.py
@@ -163,13 +163,15 @@ class PyFuseBox(Operations):
             if old in self.temp_file: 
                 self.logger.debug("flushing before renaming %s", old)
                 self.store.store_fileobject(self.temp_file[old], old)
+            if os.path.basename(old) == os.path.basename(new) and self.store._get_metadata(new)['is_dir'] and self.store._get_metadata(old)['is_dir']:
+                raise FuseOSError(ENOTEMPTY) # directory not empty
             self.store.move(old, new)
         except NoSuchFilesytemObjectError:
             raise FuseOSError(ENOENT)
         except StoreAccessError:
             raise FuseOSError(EIO)
         except StoreAutorizationError:
-            raise FuseOSError(EACCES) #keine Berechtigung
+            raise FuseOSError(EACCES)
         return 0
 
     def create(self, path, mode):

commit 09e7f62f8cee79a382bf50452d7c83781de83713

Bugfix class PyFuseBox, the bug was introduced in commit c7621bf2 and caused an error when moving a directroy to a path with the same basename

cloudfusion/pyfusebox/pyfusebox.py
@@ -163,8 +163,6 @@ class PyFuseBox(Operations):
             if old in self.temp_file: 
                 self.logger.debug("flushing before renaming %s", old)
                 self.store.store_fileobject(self.temp_file[old], old)
-            if os.path.basename(old) == os.path.basename(new) and self.store.get_metadata(new)['is_dir'] and self.store.get_metadata(old)['is_dir']:
-                raise FuseOSError(ENOTEMPTY) # directory not empty
             self.store.move(old, new)
         except NoSuchFilesytemObjectError:
             raise FuseOSError(ENOENT)