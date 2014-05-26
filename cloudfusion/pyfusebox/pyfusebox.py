joe42/CloudFusion

commit d8104c32c407a35957bcbb49d4d9b77753927bad

Bugfix method rename of class PyFuseBox, to raise an Exception with the value EISDIR if source is a file and destination is a directory

cloudfusion/pyfusebox/pyfusebox.py
@@ -159,6 +159,10 @@ class PyFuseBox(Operations):
     def rename(self, old, new):
         self.logger.debug("rename %s to %s", old, new)
         try:
+            source_is_file = not self.store.is_dir()
+            destination_is_directory = self.store.is_dir()
+            if source_is_file and destination_is_directory:
+                raise FuseOSError(EISDIR)
             #if file is opened with gedit a hidden file is written and immediately renamed to the target file without flushing 
             if old in self.temp_file: 
                 self.logger.debug("flushing before renaming %s", old)

commit a14ba17498c28f9b568ec7c6bda10b2bd6fea897

Bugfix method rename of class PyFuseBox, error introduced in commit d8104c32c40

cloudfusion/pyfusebox/pyfusebox.py
@@ -159,10 +159,13 @@ class PyFuseBox(Operations):
     def rename(self, old, new):
         self.logger.debug("rename %s to %s", old, new)
         try:
-            source_is_file = not self.store.is_dir()
-            destination_is_directory = self.store.is_dir()
-            if source_is_file and destination_is_directory:
-                raise FuseOSError(EISDIR)
+            try:
+                source_is_file = not self.store.is_dir(old)
+                destination_is_directory = self.store.is_dir(new)
+                if source_is_file and destination_is_directory:
+                    raise FuseOSError(EISDIR)
+            except NoSuchFilesytemObjectError:
+                pass # src or dst does not exist
             #if file is opened with gedit a hidden file is written and immediately renamed to the target file without flushing 
             if old in self.temp_file: 
                 self.logger.debug("flushing before renaming %s", old)