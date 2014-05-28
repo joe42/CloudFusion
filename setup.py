90b1b693ec09baf200379e76be777718f5bf94f7
Travis <travis@travis-ci.org>
2014-05-28 02:39:26 +0200

    Modify setup.py, to remove boto requirement, which is already included in gsutil

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
     name = "CloudFusion",
     packages = setuptools.find_packages(),
     include_package_data = True,
-    install_requires = ['mechanize', 'requests', 'nose', 'oauth', 'poster', 'simplejson', 'httplib2>=0.8', 'beautifulsoup4', 'argparse', 'ntplib', 'gsutil', 'sh', 'pexpect', 'boto==2.22.1', 'tinydav'],
+    install_requires = ['mechanize', 'requests', 'nose', 'oauth', 'poster', 'simplejson', 'httplib2>=0.8', 'beautifulsoup4', 'argparse', 'ntplib', 'gsutil', 'sh', 'pexpect', 'tinydav'],
     version = "6.2.15",
     description = "Filesystem interface to cloud storage services",
     author = "Johannes Mueller",
