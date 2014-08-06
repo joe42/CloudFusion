88a5fbc25fa885c73c108c8a54bee68361caa230
Johannes Müller <quirksquarks@web.de>
2014-08-06 16:53:08 +0200

    Modify class ConfigurablePyFuseBox, move responsibility for configuring the store instance to class VirtualConfigFile Add parameter pyfusebox to constructor of class VirtualConfigFile, to be able to configure the PyFuseBox instance according to the contents of the configuration file

--- cloudfusion/pyfusebox/configurable_pyfusebox.py
+++ cloudfusion/pyfusebox/configurable_pyfusebox.py
@@ -6,19 +6,15 @@ This FUSE module initializes the store at runtime when the user accesses the vir
 
 from cloudfusion.pyfusebox.pyfusebox import *
 from cloudfusion.pyfusebox.virtualconfigfile import VirtualConfigFile
-from cloudfusion.store.transparent_caching_store import TransparentMultiprocessingCachingStore
-from cloudfusion.store.metadata_caching_store import MetadataCachingStore
-import random
 import os, signal
 import sys
-from cloudfusion.store.transparent_chunk_caching_store import TransparentChunkMultiprocessingCachingStore
 
 
 class ConfigurablePyFuseBox(PyFuseBox):
     VIRTUAL_CONFIG_FILE = '/config/config'
     DATA_FOLDER_PATH = "/data"
     def __init__(self, root):
-        self.virtual_file = VirtualConfigFile(self.VIRTUAL_CONFIG_FILE)
+        self.virtual_file = VirtualConfigFile(self.VIRTUAL_CONFIG_FILE, self)
         self.store_initialized = False
         self.logger = logging.getLogger('pyfusebox')
         self.logger.debug("initialized configurable pyfusebox")
@@ -148,82 +144,11 @@ class ConfigurablePyFuseBox(PyFuseBox):
             path = self.remove_data_folder_prefix(path)
             return super( ConfigurablePyFuseBox, self ).read(path, size, offset, fh)
 
-    def _initialize_store(self):
-        self.logger.debug("_initialize_store:")
-        conf = self.virtual_file.get_store_config_data()
-        service = conf['name']
-        self.logger.debug("got service name")
-        cache_time = int(conf.get('cache', 0))
-        type = conf.get('type', '') #chunk
-        max_chunk_size = conf.get(('max_chunk_size', 4)) 
-        metadata_cache_time = int(conf.get('metadata_cache', 0))
-        cache_size = int(conf.get('cache_size', 2000))
-        hard_cache_size_limit = int(conf.get('hard_cache_size_limit', 10000))
-        cache_id = str(conf.get('cache_id', random.random()))
-        cache_dir = str(conf.get('cache_dir', '/tmp/cloudfusion'))
-        self.logger.debug("got cache parameter")
-        auth = self.virtual_file.get_service_auth_data()
-        auth['cache_id'] = cache_id # workaround; Dropbox needs access to cache_id to create a temporary directory with its name, to distinguish sessions
-        if 'access_key_id' in auth:
-            auth['consumer_key'] = auth['access_key_id']
-            auth['consumer_secret'] = auth['secret_access_key']
-        bucket_name = auth.get('bucket_name', 'cloudfusion') 
-        auth['bucket_name'] = bucket_name 
-        self.logger.debug("got auth data: %s" % auth)
-        config = auth
-        config['cache_dir'] = cache_dir
-        store = self.__get_new_store(service, config) #catch error?
-        self.logger.debug("initialized store")
-        if type != '':                                                      
-            store = TransparentChunkMultiprocessingCachingStore( MetadataCachingStore( store,  24*60*60*365), cache_time, cache_size, hard_cache_size_limit, cache_id, max_chunk_size, cache_dir )
-        elif cache_time > 0 and metadata_cache_time > 0:
-            store = TransparentMultiprocessingCachingStore( MetadataCachingStore( store, metadata_cache_time ), cache_time, cache_size, hard_cache_size_limit, cache_id, cache_dir )
-        elif cache_time > 0:
-            store = TransparentMultiprocessingCachingStore(store, cache_time, cache_size, hard_cache_size_limit, cache_id, cache_dir)
-        elif metadata_cache_time > 0:
-            store = MetadataCachingStore( store, metadata_cache_time )
-        self.store = store
-        self.logger.debug("initialized service")
-        self.store_initialized = True
-        
-    def __get_new_store(self, service, auth):
-        '''To add a new implementation of :class:`cloudfusion.store.Store`, add an elif branch in the if statement.
-        The parameter service is the value of the variable name specified in the configuration file in the [store] section.
-        The parameter auth is a dictionary with every variable specified in the configuration file in the [auth] section, such as password and user.
-        :param service: The name of the service to be used. I.e. Sugarsync, Dropbox, or Google Storage.
-        :param auth: Dictionary of the variable specified in the configuration file's [auth] section.'''
-        self.logger.debug("__get_new_store:")
-        try:
-            if service.lower() == "sugarsync":
-                from cloudfusion.store.sugarsync.sugarsync_store import SugarsyncStore
-                store = SugarsyncStore(auth)
-            elif service.lower() == "gs" or service.find('oogle') >= 0:
-                from cloudfusion.store.gs.bulk_get_metadata_google_store import BulkGetMetadataGoogleStore
-                store = BulkGetMetadataGoogleStore(auth)
-            elif service.lower() == "s3" or service.find('mazon') >= 0:
-                from cloudfusion.store.s3.bulk_get_metadata_amazon_store import BulkGetMetadataAmazonStore
-                store = BulkGetMetadataAmazonStore(auth)
-            elif service.lower() == "webdav" or service.find('dav') >= 0:
-                from cloudfusion.store.webdav.bulk_get_metadata_webdav_store import BulkGetMetadataWebdavStore
-                store = BulkGetMetadataWebdavStore(auth)
-            else: # default
-                from cloudfusion.store.dropbox.dropbox_store import DropboxStore
-                store = DropboxStore(auth)
-            self.logger.debug("got store")
-        except Exception as e:
-            import traceback
-            self.logger.debug(traceback.format_exc())
-            raise e
-        return store
-    
     def write(self, path, buf, offset, fh):
         #self.logger.debug("write %s ... starting with %s at %s - fh: %s", path, buf[0:10], offset, fh)
         if path == self.virtual_file.get_path():
             self.logger.debug("writing to virtual file %s", path)
             written_bytes = self.virtual_file.write(buf, offset)
-            self.logger.debug("written bytes:%s", written_bytes)
-            if written_bytes >0: # configuration changed
-                self._initialize_store()
             return written_bytes
         if self.store_initialized and path.startswith(self.DATA_FOLDER_PATH):
             path = self.remove_data_folder_prefix(path)
--- cloudfusion/pyfusebox/virtualconfigfile.py
+++ cloudfusion/pyfusebox/virtualconfigfile.py
@@ -4,6 +4,10 @@ Created on 24.08.2011
 from ConfigParser import SafeConfigParser
 from cloudfusion.pyfusebox.virtualfile import VirtualFile
 from cloudfusion.store.dropbox import file_decorator
+from cloudfusion.store.transparent_caching_store import TransparentMultiprocessingCachingStore
+from cloudfusion.store.metadata_caching_store import MetadataCachingStore
+import random
+from cloudfusion.store.transparent_chunk_caching_store import TransparentChunkMultiprocessingCachingStore
 
 class VirtualConfigFile(VirtualFile):
     INITIAL_TEXT="""
@@ -12,8 +16,9 @@ class VirtualConfigFile(VirtualFile):
 #explanation of config parameter 3
 
 """
-    def __init__(self, path):
+    def __init__(self, path, pyfusebox):
         super( VirtualConfigFile, self ).__init__(path)
+        self.pyfusebox = pyfusebox
         
     def get_service_auth_data(self):
         vtf = file_decorator.DataFileWrapper(self.get_text())
@@ -27,13 +32,79 @@ class VirtualConfigFile(VirtualFile):
         config.readfp(vtf)
         return dict(config.items('store'))
     
-    #def __check_data(self, text):
-    #    """::returns: True iff text contains all necessary parameters in the right format and False otherwise."""
-    #    return True
-
-    """def write(self, buf, offset):
-        text_tmp = self.text[:offset]+buf+self.text[len(buf)+offset:] 
-        if self.__check_data(text_tmp):
-            self.text = text_tmp
-            return len(buf)
-        return 0"""
+    def write(self, buf, offset):
+        written_bytes = super(VirtualConfigFile, self).write(buf, offset)
+        if written_bytes >0: # configuration changed
+            self._initialize_store()
+        return written_bytes
+    
+    def _initialize_store(self):
+        '''Parametrize the store implementation with the settings in the configuration file
+        Also, it is determined which wrappers should envelope the store for caching,
+        or to provide a monitoring layer. '''
+        self.logger.debug("_initialize_store:")
+        conf = self.get_store_config_data()
+        service = conf['name']
+        self.logger.debug("got service name")
+        cache_time = int(conf.get('cache', 0))
+        type = conf.get('type', '') #chunk
+        max_chunk_size = conf.get(('max_chunk_size', 4)) 
+        metadata_cache_time = int(conf.get('metadata_cache', 0))
+        cache_size = int(conf.get('cache_size', 2000))
+        hard_cache_size_limit = int(conf.get('hard_cache_size_limit', 10000))
+        cache_id = str(conf.get('cache_id', random.random()))
+        cache_dir = str(conf.get('cache_dir', '/tmp/cloudfusion'))
+        self.logger.debug("got cache parameter")
+        auth = self.get_service_auth_data()
+        auth['cache_id'] = cache_id # workaround; Dropbox needs access to cache_id to create a temporary directory with its name, to distinguish sessions
+        if 'access_key_id' in auth:
+            auth['consumer_key'] = auth['access_key_id']
+            auth['consumer_secret'] = auth['secret_access_key']
+        bucket_name = auth.get('bucket_name', 'cloudfusion') 
+        auth['bucket_name'] = bucket_name 
+        self.logger.debug("got auth data: %s" % auth)
+        config = auth
+        config['cache_dir'] = cache_dir
+        store = self.__get_new_store(service, config) #catch error?
+        self.logger.debug("initialized store")
+        if type != '':                                                      
+            store = TransparentChunkMultiprocessingCachingStore( MetadataCachingStore( store,  24*60*60*365), cache_time, cache_size, hard_cache_size_limit, cache_id, max_chunk_size, cache_dir )
+        elif cache_time > 0 and metadata_cache_time > 0:
+            store = TransparentMultiprocessingCachingStore( MetadataCachingStore( store, metadata_cache_time ), cache_time, cache_size, hard_cache_size_limit, cache_id, cache_dir )
+        elif cache_time > 0:
+            store = TransparentMultiprocessingCachingStore(store, cache_time, cache_size, hard_cache_size_limit, cache_id, cache_dir)
+        elif metadata_cache_time > 0:
+            store = MetadataCachingStore( store, metadata_cache_time )
+        self.pyfusebox.store = store
+        self.logger.debug("initialized service")
+        self.pyfusebox.store_initialized = True
+        
+    def __get_new_store(self, service, auth):
+        '''To add a new implementation of :class:`cloudfusion.store.Store`, add an elif branch in the if statement.
+        The parameter service is the value of the variable name specified in the configuration file in the [store] section.
+        The parameter auth is a dictionary with every variable specified in the configuration file in the [auth] section, such as password and user.
+        :param service: The name of the service to be used. I.e. Sugarsync, Dropbox, or Google Storage.
+        :param auth: Dictionary of the variable specified in the configuration file's [auth] section.'''
+        self.logger.debug("__get_new_store:")
+        try:
+            if service.lower() == "sugarsync":
+                from cloudfusion.store.sugarsync.sugarsync_store import SugarsyncStore
+                store = SugarsyncStore(auth)
+            elif service.lower() == "gs" or service.find('oogle') >= 0:
+                from cloudfusion.store.gs.bulk_get_metadata_google_store import BulkGetMetadataGoogleStore
+                store = BulkGetMetadataGoogleStore(auth)
+            elif service.lower() == "s3" or service.find('mazon') >= 0:
+                from cloudfusion.store.s3.bulk_get_metadata_amazon_store import BulkGetMetadataAmazonStore
+                store = BulkGetMetadataAmazonStore(auth)
+            elif service.lower() == "webdav" or service.find('dav') >= 0:
+                from cloudfusion.store.webdav.bulk_get_metadata_webdav_store import BulkGetMetadataWebdavStore
+                store = BulkGetMetadataWebdavStore(auth)
+            else: # default
+                from cloudfusion.store.dropbox.dropbox_store import DropboxStore
+                store = DropboxStore(auth)
+            self.logger.debug("got store")
+        except Exception as e:
+            import traceback
+            self.logger.debug(traceback.format_exc())
+            raise e
+        return store
