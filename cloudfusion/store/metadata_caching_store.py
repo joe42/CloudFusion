c0e45742046e03a6b6829b6d7b63ad20ff0496ee
Johannes Müller <quirksquarks@web.de>
2014-10-13 00:02:52 +0200

    Bugfix class MetadataCachingStore, do not include the directory itself in the listing

--- cloudfusion/store/metadata_caching_store.py
+++ cloudfusion/store/metadata_caching_store.py
@@ -278,7 +278,7 @@ class MetadataCachingStore(Store):
         '''Add existing files or directories to *dir_entry* because they might have been 
         uploaded recently and might not be retrievable by a directory listing from the storage provider.'''
         for path in self.entries.get_keys():
-            if os.path.dirname(path) == dir_entry_path:
+            if os.path.dirname(path) == dir_entry_path and path != '/':
                 if not self.entries.is_expired(path):
                     dir_entry.add_to_listing(path)
     
