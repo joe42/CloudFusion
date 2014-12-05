22dbbf6f2227b3b3e2a26fbe29c52b4bf2d50aa6
Johannes Müller <quirksquarks@web.de>
2014-12-05 19:02:17 +0100

    Bugfix script run_tests.sh, correctly place credentials into google storage configuration

--- buildscripts/run_tests.sh
+++ buildscripts/run_tests.sh
@@ -35,8 +35,8 @@ perl -pi -e "s/user = /user = ${USR}/g" cloudfusion/config/Dropbox.ini
 perl -pi -e "s/password = /password = ${PW}/g" cloudfusion/config/Dropbox.ini
 perl -pi -e "s/user = /user = ${USR}/g" cloudfusion/config/sugarsync_testing.ini
 perl -pi -e "s/password = /password = ${PW}/g" cloudfusion/config/sugarsync_testing.ini
-perl -pi -e "s/access_key_id =.*/access_key_id =${GS_ID}/g" cloudfusion/config/Google_testing.ini
-perl -pi -e "s/secret_access_key =.*/secret_access_key =${GS_KEY}/g" cloudfusion/config/Google_testing.ini
+perl -pi -e "s/client_id =.*/client_id =${GS_ID}/g" cloudfusion/config/Google_testing.ini
+perl -pi -e "s/client_secret =.*/client_secret =${GS_KEY}/g" cloudfusion/config/Google_testing.ini
 perl -pi -e "s/access_key_id =.*/access_key_id =${S3_ID}/g" cloudfusion/config/AmazonS3_testing.ini
 perl -pi -e "s/secret_access_key =.*/secret_access_key =${S3_KEY}/g" cloudfusion/config/AmazonS3_testing.ini
 perl -pi -e "s/user =.*/user =${WEBDAV_USR}/g" cloudfusion/config/Webdav_tonline_testing.ini
@@ -47,7 +47,6 @@ perl -pi -e "s/user =.*/user =${WEBDAV3_USR}/g" cloudfusion/config/Webdav_box_te
 perl -pi -e "s/password =.*/password =${WEBDAV3_PWD}/g" cloudfusion/config/Webdav_box_testing.ini
 perl -pi -e "s/user =.*/user =${WEBDAV4_USR}/g" cloudfusion/config/Webdav_yandex_testing.ini
 perl -pi -e "s/password =.*/password =${WEBDAV4_PWD}/g" cloudfusion/config/Webdav_yandex_testing.ini
-
 
 perl -pi -e "s/bucket_name =.*/bucket_name = cloudfusion/g" cloudfusion/config/Google_testing.ini
 perl -pi -e "s/bucket_name =.*/bucket_name = cloudfusion/g" cloudfusion/config/AmazonS3_testing.ini
