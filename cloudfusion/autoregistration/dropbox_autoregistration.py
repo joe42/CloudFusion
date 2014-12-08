38ff347a30d6af014d60e4d07cf48feb57d58b46
Johannes Müller <quirksquarks@web.de>
2014-12-08 23:22:59 +0100

    Hotfix script dropbox_autoregistration.py, update to user interface change of Dropbox website

--- cloudfusion/autoregistration/dropbox_autoregistration.py
+++ cloudfusion/autoregistration/dropbox_autoregistration.py
@@ -19,17 +19,15 @@ Settings.MoveMouseDelay = 0
 SUPPORTED_LANGUAGES = ['english','german']
 dbRegistrationBtn = "Registration_Btn.png"
 firstNameInput = "First_Name_Input.png"
-continueBtnFreeAccount = "Continue_Btn_Free_Account.png"
 def configure_language():
     '''Set the correct images according to the language of the website in the current browser window.
     :return: True iff successful'''
-    global dbRegistrationBtn, firstNameInput, continueBtnFreeAccount
+    global dbRegistrationBtn, firstNameInput
     for lang in SUPPORTED_LANGUAGES:
         for tries in range(3):
             if screen.exists(lang+'/'+dbRegistrationBtn,2):
                 dbRegistrationBtn = lang + '/' + dbRegistrationBtn
                 firstNameInput = lang +'/' + firstNameInput
-                continueBtnFreeAccount = lang + '/' + continueBtnFreeAccount
                 return True
     return False
 
@@ -72,12 +70,4 @@ if __name__ == '__main__':
     screen.type(Key.TAB)
     screen.type(Key.SPACE)
     screen.click(dbRegistrationBtn)
-    if not screen.exists(continueBtnFreeAccount,4):
-        popup("Automatic registration at Dropbox failed for one of the following reasons.\n"+
-              "You can report an issue at https://github.com/joe42/CloudFusion/issues.\n"+
-              "1. The Dropbox registration user interface has changed\n"+
-              "2. You or another application interfered with the registration process\n", 
-              "Error")
-    screen.wait()
-    screen.click(continueBtnFreeAccount)
     start_browser("http://www.dropbox.com")
