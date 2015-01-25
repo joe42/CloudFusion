bded766c62771b801eb335bfe4b3c53ff08adcb3
Johannes Müller <quirksquarks@web.de>
2015-01-25 21:23:58 +0100

    fix build

--- buildscripts/run_tests.sh
+++ buildscripts/run_tests.sh
@@ -9,7 +9,9 @@ if [ "$TEST_SUITE" = "integration" ] ; then
 else
     git clone https://github.com/joe42/fusetests.git /tmp/fusetests
     echo "$TRAVIS_BUILD_DIR" > /tmp/TRAVIS_BUILD_DIR
-    /usr/bin/linux.uml init=`pwd`/buildscripts/run_system_tests.sh rootfstype=hostfs rw eth0=slirp
+    # Start User Mode Linux with root privileges 
+    # to work around multiprocessing bug which requires removing /dev/shm.
+    sudo /usr/bin/linux.uml init=`pwd`/buildscripts/run_system_tests.sh rootfstype=hostfs rw eth0=slirp
     exit $(<"$TRAVIS_BUILD_DIR/fusetest.status") 
 fi
 
