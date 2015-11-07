0035d0b2b4a66f49775118dd4d7f1b28b23de45b
Johannes Müller <quirksquarks@web.de>
2015-11-07 23:05:06 +0100

    wait for ec2 instance to be running

--- buildscripts/start_ec2_instance.sh
+++ buildscripts/start_ec2_instance.sh
@@ -1,11 +1,15 @@
 . buildscripts/ec2_variables.sh
 ec2-run-instances ami-57360a20 -t t1.micro --region eu-west-1 -k $key -g sg-8ec476eb  
-IP=$(ec2-describe-instances|awk '{ if($1=="PRIVATEIPADDRESS") { print $4 } }')
-# Wait for instance to be running. 
-while ! ping -c 1 $IP &> /dev/null; do
+# Wait for instance to be running.
+tries=20
+while ! ec2-describe-instances|grep running  &> /dev/null; do
+  let tries=tries-1
+  if [ tries -eq 0 ] ; then
+    echo Waited too long for ec2 instance to start. Continuing without further waiting.
+    break
+  fi
   sleep 5
-  IP=$(ec2-describe-instances|awk '{ if($1=="PRIVATEIPADDRESS") { print $4 } }')
 done
-sleep 10
+IP=$(ec2-describe-instances|awk '{ if($1=="PRIVATEIPADDRESS") { print $4 } }')
 echo $IP
 
