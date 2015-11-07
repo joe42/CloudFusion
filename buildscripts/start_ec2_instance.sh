7145b1fa7b7a02b0384655a781d5b0985e1da847
Johannes Müller <quirksquarks@web.de>
2015-11-07 23:34:22 +0100

    wait for ec2 instance to be running

--- buildscripts/start_ec2_instance.sh
+++ buildscripts/start_ec2_instance.sh
@@ -2,9 +2,9 @@
 ec2-run-instances ami-57360a20 -t t1.micro --region eu-west-1 -k $key -g sg-8ec476eb  
 # Wait for instance to be running.
 tries=20
-while ! ec2-describe-instances|grep running  &> /dev/null; do
+while ! ec2-describe-instances|grep -q running; do
   let tries=tries-1
-  if [ tries -eq 0 ] ; then
+  if [ $tries -eq 0 ] ; then
     echo Waited too long for ec2 instance to start. Continuing without further waiting.
     break
   fi
