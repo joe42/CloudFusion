f065bad82bd1bf7e2d7e26a9e8e772338a73f5e1
Johannes Müller <quirksquarks@web.de>
2015-11-07 22:34:35 +0100

    wait for ec2 instance to be running and fix buildscript path

--- buildscripts/start_ec2_instance.sh
+++ buildscripts/start_ec2_instance.sh
@@ -1,6 +1,11 @@
 . buildscripts/ec2_variables.sh
 ec2-run-instances ami-57360a20 -t t1.micro --region eu-west-1 -k $key -g sg-8ec476eb  
-sleep 10;
 IP=$(ec2-describe-instances|awk '{ if($1=="PRIVATEIPADDRESS") { print $4 } }')
+# Wait for instance to be running. 
+while ! ping -c 1 $IP &> /dev/null; do
+  sleep 5
+  IP=$(ec2-describe-instances|awk '{ if($1=="PRIVATEIPADDRESS") { print $4 } }')
+done
+sleep 10
 echo $IP
 
--- buildscripts/terminate_ec2_instance.sh
+++ buildscripts/terminate_ec2_instance.sh
@@ -1,3 +1,3 @@
-. ec2_variables.sh
+. buildscripts/ec2_variables.sh
 ID=$(ec2-describe-instances|awk '{ if($1=="INSTANCE") { print $2 } }')
 ec2-terminate-instances $ID
