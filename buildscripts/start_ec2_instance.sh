0ba5e3fd7e48bea5d175cd184d75d8a12c745ada
Johannes Müller <quirksquarks@web.de>
2015-11-07 22:13:39 +0100

    ec2-tools installed on travis require the id of the security group instead of the group name

--- buildscripts/start_ec2_instance.sh
+++ buildscripts/start_ec2_instance.sh
@@ -1,5 +1,5 @@
 . buildscripts/ec2_variables.sh
-ec2-run-instances ami-57360a20 -t t1.micro --region eu-west-1 -k $key -g me2  
+ec2-run-instances ami-57360a20 -t t1.micro --region eu-west-1 -k $key -g sg-8ec476eb  
 sleep 10;
 IP=$(ec2-describe-instances|awk '{ if($1=="PRIVATEIPADDRESS") { print $4 } }')
 echo $IP
