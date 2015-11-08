02c08a6cd84b8ab76b14f42b79b2044338498b6d
Johannes Müller <quirksquarks@web.de>
2015-11-08 10:10:14 +0100

    fix invalid redirect

--- buildscripts/start_ec2_instance.sh
+++ buildscripts/start_ec2_instance.sh
@@ -2,7 +2,7 @@
 ec2-run-instances ami-57360a20 -t t1.micro --region eu-west-1 -k $key -g sg-8ec476eb  
 # Wait for instance to be running.
 tries=20
-while ! ec2-describe-instances|grep -q running >$2; do
+while ! ec2-describe-instances|grep -q running >&2; do
   let tries=tries-1
   if [ $tries -eq 0 ] ; then
     break
