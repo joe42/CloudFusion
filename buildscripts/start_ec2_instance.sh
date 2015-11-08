e77a8ed7cee96ad83ace00ec489fdb11fc1d902e
Johannes Müller <quirksquarks@web.de>
2015-11-08 10:54:21 +0100

    wait for ec2 instance to be running and fix buildscript path

--- buildscripts/start_ec2_instance.sh
+++ buildscripts/start_ec2_instance.sh
@@ -1,10 +1,10 @@
 . buildscripts/ec2_variables.sh
-ec2-run-instances ami-57360a20 -t t1.micro --region eu-west-1 -k $key -g sg-8ec476eb  
 # Wait for instance to be running.
-tries=20
+tries=40
 while ! ec2-describe-instances|grep -q running >&2; do
   let tries=tries-1
   if [ $tries -eq 0 ] ; then
+    echo Waited too long for EC2 instance to start.>&2
     break
   fi
   sleep 5
