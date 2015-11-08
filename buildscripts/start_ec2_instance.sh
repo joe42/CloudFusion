2f39a336204aef013511c151e397068bfa006032
Johannes Müller <quirksquarks@web.de>
2015-11-08 12:27:57 +0100

    wait for ec2 instance to be running and fix buildscript path

--- buildscripts/start_ec2_instance.sh
+++ buildscripts/start_ec2_instance.sh
@@ -1,9 +1,9 @@
 . buildscripts/ec2_variables.sh
-echo Starting EC2 instance.
-ec2-run-instances ami-57360a20 -t t1.micro --region eu-west-1 -k $key -g sg-8ec476eb  >&2
-# Wait for instance to be running.
+echo Starting EC2 instance. >&2
+ec2-run-instances ami-57360a20 -t t1.micro --region eu-west-1 -k $key -g sg-8ec476eb >&2
+# Wait for instance to be accessible.
 tries=40
-while ! ec2-describe-instances|grep -q running; do
+while ! ec2-describe-instances|grep -q PRIVATEIPADDRESS; do
   let tries=tries-1
   if [ $tries -eq 0 ] ; then
     echo Waited too long for EC2 instance to start:>&2
@@ -12,6 +12,7 @@ while ! ec2-describe-instances|grep -q running; do
   fi
   sleep 5
 done
+echo EC2 instance details: >&2
 ec2-describe-instances >&2
 sleep 10
 IP=$(ec2-describe-instances|awk '{ if($1=="PRIVATEIPADDRESS") { print $4 } }')
