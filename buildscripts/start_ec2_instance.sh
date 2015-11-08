4b6db49a6b2d5988bac87dc220c327eb8568c9bc
Johannes Müller <quirksquarks@web.de>
2015-11-08 12:52:41 +0100

    adapt parsing to different output produced by ec2-tools

--- buildscripts/start_ec2_instance.sh
+++ buildscripts/start_ec2_instance.sh
@@ -3,7 +3,7 @@ echo Starting EC2 instance. >&2
 ec2-run-instances ami-57360a20 -t t1.micro --region eu-west-1 -k $key -g sg-8ec476eb >&2
 # Wait for instance to be accessible.
 tries=40
-while ! ec2-describe-instances|grep -q PRIVATEIPADDRESS; do
+while ! ec2-describe-instances|grep -q running; do
   let tries=tries-1
   if [ $tries -eq 0 ] ; then
     echo Waited too long for EC2 instance to start:>&2
@@ -15,7 +15,7 @@ done
 echo EC2 instance details: >&2
 ec2-describe-instances >&2
 sleep 10
-IP=$(ec2-describe-instances|awk '{ if($1=="PRIVATEIPADDRESS") { print $4 } }')
+IP=$(ec2-describe-instances|awk '{ if($1=="INSTANCE") { print $4; exit 0 } }')
 echo IP:$IP >&2
 echo $IP
 
